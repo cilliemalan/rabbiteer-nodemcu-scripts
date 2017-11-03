0ec4efa56b56725da3796c2e15ab72abfa075269
Cillié Malan <cilliemalan@gmail.com>
2017-11-03 07:43:45 +0200

    Improved mqtt a bit

--- mqtt.lua
+++ mqtt.lua
@@ -5,18 +5,15 @@ password = "password"
 url = "iot.rabbiteer.io"
 
 -- the mqtt client
-m = mqtt.Client(client, 60, client, password);
+m = mqtt.Client(
+    client,    -- client id
+    60,        -- keepalive
+    client,    -- username
+    password); -- password
 
 -- handlers
-function on_mqtt_online(client) print("online") end
-function on_mqtt_offline(client) print("offline") end
 function on_mqtt_message(client,topic,message) print("got message on " .. topic .. ": " .. message) end
 function on_mqtt_error(client, reason) print("error"); print(reason);end
-
--- bind the handlers
-m:on("connect", on_mqtt_online)
-m:on("offline", on_mqtt_offline)
-m:on("message", on_mqtt_message)
 
 -- function to call when connected
 function on_mqtt_connected(m)
@@ -27,15 +24,15 @@ function on_mqtt_connected(m)
     m:publish("/slack", "hello from " .. client, 0, 0)
 end
 
--- connect
-m:connect(url, 8883, 1, 0, on_mqtt_connected, on_mqtt_error)
+-- bind the handlers
+m:on("message", on_mqtt_message)
 
--- function to call when connected
-function on_mqtt_connected(client)
-    print("connected")
-    -- subscribe to a channel
-    m:subscribe("/slack", 0)
-    -- publish a message to a channel
-    m:publish("/slack", "hello from " .. client, 0, 0)
-end
+-- connect
+m:connect(
+    url,                -- host
+    8883,               -- port
+    1,                  -- secure
+    0,                  -- autoreconnect
+    on_mqtt_connected,  -- connect callback
+    on_mqtt_error)      -- error callback
 
