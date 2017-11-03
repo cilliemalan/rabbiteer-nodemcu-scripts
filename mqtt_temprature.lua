e5645684e591dca33f73216b5f6f92355590395f
Cillié Malan <cilliemalan@gmail.com>
2017-11-03 13:08:45 +0200

    Fixed publish url

--- mqtt_temprature.lua
+++ mqtt_temprature.lua
@@ -9,29 +9,24 @@ sda = 1
 scl = 2
 
 function on_mqtt_connected(m)
-
     -- read temprature and humidity from the AM2302
     gpio.mode(sda, gpio.OUTPUT, 1)
     gpio.mode(scl, gpio.OUTPUT, 2)
-
     --setup i2c and connect to the device
     i2c.setup(0, sda, scl, i2c.SLOW)
     am2320.setup()
-
     -- take a reading every 5 seconds and send via mqtt
     tmr.create():alarm(5000, tmr.ALARM_AUTO, function()
         rh, t = am2320.read()
-
         m:publish(
-            "/metric/temprature/" .. client,
+            "metric/temprature/" .. client,
             string.format("%s", t/10),
             0, 0)
         m:publish(
-            "/metric/humidity/" .. client,
+            "metric/humidity/" .. client,
             string.format("%s", rh/10),
             0, 0)
     end)
-
 end
 
 function on_mqtt_error(client, reason) print("error"); print(reason);end
