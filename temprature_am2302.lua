b4e2eacbce7aed04eab35370f97eb13c913f361e
Cillié Malan <cilliemalan@gmail.com>
2017-11-09 08:30:37 +0200

    tweaked the tenp script

--- temprature_am2302.lua
+++ temprature_am2302.lua
@@ -1,19 +1,20 @@
--- read temprature and humidity from the AM2302
+-- This script reads temprature and humidity from the AM2302
+-- every 5 seconds and prints it out.
 
---data and clock pins
-sda = 1
-scl = 2
-
-gpio.mode(sda, gpio.OUTPUT, 1)
-gpio.mode(scl, gpio.OUTPUT, 2)
+--these are the data and clock pin numbers
+sda, scl = 1, 2
 
 --setup i2c and connect to the device
-i2c.setup(0, sda, scl, i2c.SLOW)
-am2320.setup()
+i2c.setup(0, sda, scl, i2c.SLOW);
+am2320.setup();
 
--- take a reading every 5 seconds and print
-tmr.create():alarm(5000, tmr.ALARM_AUTO, function()
+function gettemp()
     rh, t = am2320.read()
     print(string.format("Humidity: %s%%  Temperature: %s", rh / 10, t / 10))
-end)
+end
 
+-- take a reading right now and print
+gettemp();
+
+-- take a reading every 5 seconds and print
+tmr.create():alarm(5000, tmr.ALARM_AUTO, gettemp)
