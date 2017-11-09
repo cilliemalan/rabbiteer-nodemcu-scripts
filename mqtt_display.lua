fe90618b3d6a1299dcc18b0495a0a1ca57e1a65e
Cillié Malan <cilliemalan@gmail.com>
2017-11-09 10:04:50 +0200

    tweaked mqtt display

--- mqtt_display.lua
+++ mqtt_display.lua
@@ -2,11 +2,12 @@
 client = "client"
 password = "password"
 url = "iot.rabbiteer.io"
-sensor = "harrier"
+
+-- the sensor to listen for temprature updates from
+sensor = "cillie"
 
 -- data and clock pins for sensor
-sda = 1
-scl = 2
+sda, scl = 1, 2
 
 -- init OLED display
 sla = 0x3C
@@ -16,12 +17,12 @@ disp:setFont(u8g.font_6x10)
 disp:setFontRefHeightExtendedText()
 disp:setDefaultForegroundColor()
 disp:setFontPosTop()
---Rotate Display if needed
---disp:setRot180()
 
+-- initial temp and humi
 temp = "__._"
 humi = "__._"
 
+-- print the current temp and humi
 function print_OLED()
     disp:firstPage()
     repeat
@@ -31,22 +32,27 @@ function print_OLED()
     until disp:nextPage() == false
 end
 
-print_OLED()
-
+-- topics to listen for updates from
 temp_topic = "metric/temprature/" .. sensor;
 humi_topic = "metric/humidity/" .. sensor;
 
+-- this will be called when an MQTT message is received
 function on_mqtt_message(client,topic,message)
+    -- update temp if it's temp topic
     if topic == temp_topic then
         temp = message;
     end
+
+    -- update humi if it's humi topic
     if topic == humi_topic then
         humi = message;
     end
+
+    -- update display
     print_OLED()
 end
 
--- mqtt connect handler
+-- this will be called once mqtt is connected
 function on_mqtt_connected(m)
     print("connected");
     -- subscribe to temp & humi channels
@@ -54,9 +60,16 @@ function on_mqtt_connected(m)
     m:subscribe(humi_topic, 0)
 end
 
--- listen mqtt
-function on_mqtt_error(client, reason) print("error"); print(reason);end
+-- this will be called on mqtt error
+function on_mqtt_error(client, reason)
+    print("error: " .. reason);
+    node.restart();
+end
+
+-- connect to mqtt server
 m = mqtt.Client(client,60,client,password);
 m:connect(url,8883,1,0,on_mqtt_connected,on_mqtt_error)
 m:on("message", on_mqtt_message)
 
+-- print initial (blank) values
+print_OLED()
